8a6bec09bed0cd33dc04e00ec8f103df2dd4b57a
odcn662hoo <jiri.hoogeveen@duo.nl>
2020-03-11 12:50:11 +0100

    improve script

--- update_bootiso.sh
+++ update_bootiso.sh
@@ -1,25 +1,32 @@
-if [ -z $1 ]; then
-	echo "Please provide a iso"
-	exit 1
+#!/bin/bash
+
+URI="http://mirror.fra10.de.leaseweb.net/centos/8/isos/x86_64/CentOS-8.1.1911-x86_64-boot.iso"
+MD5URI="http://mirror.fra10.de.leaseweb.net/centos/8/isos/x86_64/CHECKSUM"
+BASENAME=$(basename $URI)
+OPISO="/tmp/Custom_$BASENAME"
+
+ISOLOC="/root/$BASENAME"
+
+cd $(dirname $ISOLOC)
+
+curl -s $MD5URI |grep $BASENAME > $ISOLOC.check
+
+if ! sha256sum -c $ISOLOC.check; then
+	curl -#  $URI --out $ISOLOC	
+	if ! sha256sum -c $ISOLOC.check; then
+		echo "SHA265 check failed"
+		exit 1
+	fi
 fi
-
-if [ ! $1 ];then
-	echo "ISO not found"
-	exit 1
-fi
-
-
-OPISO="custum_centos8_boot.iso"
-
-URI="http://yum.tamu.edu/centos/8/isos/x86_64/CentOS-8.1.1911-x86_64-boot.iso"
-curl $URI --out /tmp/boot.iso
+cd -
 
 yum install -y isomd5sum genisoimage
 mkdir /tmp/bootiso /tmp/bootcustom
 
-mount -o loop /tmp/boot.iso /tmp/bootiso
+mount -o loop $ISOLOC /tmp/bootiso
 
 cp -r /tmp/bootiso/* /tmp/bootcustom
+
 umount /tmp/bootiso && rmdir /tmp/bootiso
 chmod -R u+w /tmp/bootcustom
 
@@ -28,6 +35,8 @@ chmod -R u+w /tmp/bootcustom
 
 sed -i 's/append\ initrd\=initrd.img/append initrd=initrd.img\ ks\=cdrom:\/ks.cfg/' /tmp/bootcustom/isolinux/isolinux.cfg
 
-cd /tmp/bootisoks && \
-mkisofs -o /tmp/$OPISO -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -V "CentOS 7 x86_64" -R -J -v -T isolinux/. .
-implantisomd5 /tmp/$OPISO
+cd /tmp/bootcustom
+mkisofs -o $OPISO -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -V "$(blkid $ISOLOC |sed  's/.*LABEL=\"\([a-zA-Z0-9_-]*\).*/\1/')" -R -J -v -T isolinux/. .
+implantisomd5 $OPISO
+rm -rf /tmp/bootcustom
+
