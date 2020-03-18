3c9ff90632c6a3be2e7ae66a9b5d06a542a9b975
odcn662hoo <jiri.hoogeveen@duo.nl>
2020-03-18 10:32:59 +0100

    update readme and added script to modify the boot.iso

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Kickstart for Centos8
+## Update CentOS Install ISO
+To use kickstart, we need to update isolinux.cfg
+
+
+Change the kickstart location in update_centos8_bootiso.sh, to te location of your kickstart file.
+
+Execute: update_centos8_bootiso.sh
+
+This will download the latest  Centos-8-*-boot.iso and creates Custom-Centos-8-*-boot.iso.
+
+Now you can boot from the Custom-Centos-8-*-boot.iso, it will start the automated installation.
---
+++ update_centos8_bootiso.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+URI="http://mirror.fra10.de.leaseweb.net/centos/8/isos/x86_64/CentOS-8.1.1911-x86_64-boot.iso"
+MD5URI="http://mirror.fra10.de.leaseweb.net/centos/8/isos/x86_64/CHECKSUM"
+BASENAME=$(basename $URI)
+OPISO="/tmp/Custom_$BASENAME"
+
+ISOLOC="/root/$BASENAME"
+
+cd $(dirname $ISOLOC)
+
+curl -s $MD5URI |grep $BASENAME > $ISOLOC.check
+
+if ! sha256sum -c $ISOLOC.check; then
+	curl -#  $URI --out $ISOLOC	
+	if ! sha256sum -c $ISOLOC.check; then
+		echo "SHA265 check failed"
+		exit 1
+	fi
+fi
+cd -
+
+yum install -y isomd5sum genisoimage
+mkdir /tmp/bootiso /tmp/bootcustom
+
+mount -o loop $ISOLOC /tmp/bootiso
+
+cp -r /tmp/bootiso/* /tmp/bootcustom
+
+umount /tmp/bootiso && rmdir /tmp/bootiso
+chmod -R u+w /tmp/bootcustom
+
+#cp /path/to/yourks.cfg /tmp/bootcustom/isolinux/ks.cfg
+
+
+sed  -i -e 's/append\ initrd\=initrd.img/append initrd=initrd.img\ ks\=https:\/\/raw.githubusercontent.com\/wica128\/ks8\/master\/centos8.ks/' \
+	-e 's/^timeout.*/timeout 100/' \
+	-e '/menu default/d' \
+	-e '/label linux/a \ \ menu default' \
+	/tmp/bootcustom/isolinux/isolinux.cfg
+
+cd /tmp/bootcustom
+mkisofs -o $OPISO -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -V "$(blkid $ISOLOC |sed  's/.*LABEL=\"\([a-zA-Z0-9_-]*\).*/\1/')" -R -J -v -T isolinux/. .
+implantisomd5 $OPISO
+rm -rf /tmp/bootcustom
+
